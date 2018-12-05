c00b051c9d7cd7ad29cf9a61253a7cf6d28c2aad
thbdias <thiagobalbino91@gmail.com>
2018-12-05 00:08:56 -0200

    relacionamento

--- mod2/agenda/app/models/contact.rb
+++ mod2/agenda/app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+    has_many :phones #possui muitos telefones
+    has_one :address #possui um endereco
+
+    has_many :contact_kinds #possui muitos <tabela intermediaria>
+    has_many :kinds, through: :contact_kinds #possui muitos kinds através da <tabela intermediaria>
 end
--- mod2/agenda/app/models/kind.rb
+++ mod2/agenda/app/models/kind.rb
@@ -1,2 +1,4 @@
 class Kind < ApplicationRecord
+    has_many :contact_kinds # possui muitos <tabela intermediaria>
+    has_many :contacts, through: :contact_kinds # possui muitos contacts atravé da <tabela intermediaria>
 end
